a17aa1932205650e4b7552aeedbafc1970a14d1d
Nir Goldman <refaelgold@gmail.com>
2016-02-18 18:40:23 +0200

    Class Based Views

--- questions/urls.py
+++ questions/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from questions import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-
 urlpatterns = [
-    url(r'^questions/$', views.question_list),
-    url(r'^questions/(?P<pk>[0-9]+)/$', views.question_detail),
+    url(r'^questions/$', views.QuestionList.as_view()),
+    url(r'^questions/(?P<pk>[0-9]+)/$', views.QuestionDetail.as_view()),
 ]
+
+
 urlpatterns = format_suffix_patterns(urlpatterns)
