9acef2c91ec6cace225bc61c3b8abd21bec43d2a
Nir Goldman <refaelgold@gmail.com>
2016-02-19 18:28:58 +0200

    revert test

--- moonshot/urls.py
+++ moonshot/urls.py
@@ -24,6 +24,6 @@ router.register(r'users', UserViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    # url(r'^', include('questions.urls')),
+    url(r'^', include('questions.urls')),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
