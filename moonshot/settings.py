fe495e3e9538064b3a390324ac0ca2c0b9722fe1
Nir Goldman <refaelgold@gmail.com>
2016-02-20 00:16:56 +0200

    change static file location

--- moonshot/settings.py
+++ moonshot/settings.py
@@ -123,6 +123,10 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
